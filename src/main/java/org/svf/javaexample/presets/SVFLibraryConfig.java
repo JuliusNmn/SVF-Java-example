305d24a78713e844674cd14abc5b75f348bcd054
Taylor Graham <taylor.w.graham@gmail.com>
2022-04-19 17:08:58 +1000

    Initial commit with generated and manual javacpp examples

---
+++ src/main/java/org/svf/javaexample/presets/SVFLibraryConfig.java
@@ -0,0 +1,28 @@
+package org.svf.javaexample.presets;
+
+import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.Properties;
+import org.bytedeco.javacpp.tools.InfoMap;
+import org.bytedeco.javacpp.tools.InfoMapper;
+
+@Properties(
+        target = "org.svf.javaexample.generated.SVFLibrary",
+        value = @Platform(
+                includepath = {
+                        "~/node_modules/llvm-13.0.0.obj/include",
+                        "~/node_modules/svf-lib/svf-osx/include"
+                },
+                linkpath = {"~/node_modules/SVF/Release-build/lib/"},
+                include = {
+                        "SVF-FE/LLVMModule.h",
+                        "Util/SVFModule.h",
+                },
+                preload = { "libSvf" },
+                compiler = { "cpp14" }
+        )
+)
+public class SVFLibraryConfig implements InfoMapper {
+    @Override
+    public void map(InfoMap infoMap) {
+    }
+}
